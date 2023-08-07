80bfc4a3cf49d91567e2090800a68992c3a89d33
aruuleon <arieleonchang@gmail.com>
2023-08-07 09:18:01 -0300

    0002

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -70,7 +70,7 @@ const controller = {
     return {
       id,
       client,
-      [client]: {
+      personalization: {
         ".splashPersonalizado": {
          "background-image": arrayAssets[0]
         },
@@ -102,18 +102,17 @@ const controller = {
     }
   },
   personalizationUpdate: function (req, clientFound, arrayAssets) {
-    let client = clientFound.client;
-    clientFound[client][".splashPersonalizado"]["background-image"] = arrayAssets[0];
-    clientFound[client][".backgroundPersonalizado"]["background-image"] = arrayAssets[1];
-    clientFound[client][".logoPersonalizado"]["content"] = arrayAssets[2];
-    clientFound[client][".logoPersonalizado"]["width"] = req.body.widthLogo;
-    clientFound[client][".contenedorLogoPersonalizado"]["justify-content"] = req.body.justifyContentContenedorLogo;
-    clientFound[client][".contenedorHoraPersonalizado"]["display"] = req.body.displayContenedorHora;
-    clientFound[client][".temaPersonalizado"]["color"] = req.body.colorTema;
-    clientFound[client][".temaPersonalizado"]["background-color"] = req.body.backgroundColorTema;
-    clientFound[client][".botonDeAnunciosPersonalizado"]["color"] = req.body.colorBotonDeAnuncios;
-    clientFound[client][".botonDeAnunciosPersonalizado"]["background-color"] = req.body.backgroundColorBotonDeAnuncios;
-    clientFound[client][".colorLetraAnunciosPersonalizado"]["color"] = req.body.colorLetraAnuncios;
+    clientFound["personalization"][".splashPersonalizado"]["background-image"] = arrayAssets[0];
+    clientFound["personalization"][".backgroundPersonalizado"]["background-image"] = arrayAssets[1];
+    clientFound["personalization"][".logoPersonalizado"]["content"] = arrayAssets[2];
+    clientFound["personalization"][".logoPersonalizado"]["width"] = req.body.widthLogo;
+    clientFound["personalization"][".contenedorLogoPersonalizado"]["justify-content"] = req.body.justifyContentContenedorLogo;
+    clientFound["personalization"][".contenedorHoraPersonalizado"]["display"] = req.body.displayContenedorHora;
+    clientFound["personalization"][".temaPersonalizado"]["color"] = req.body.colorTema;
+    clientFound["personalization"][".temaPersonalizado"]["background-color"] = req.body.backgroundColorTema;
+    clientFound["personalization"][".botonDeAnunciosPersonalizado"]["color"] = req.body.colorBotonDeAnuncios;
+    clientFound["personalization"][".botonDeAnunciosPersonalizado"]["background-color"] = req.body.backgroundColorBotonDeAnuncios;
+    clientFound["personalization"][".colorLetraAnunciosPersonalizado"]["color"] = req.body.colorLetraAnuncios;
   }
 };
 
